0dbf942e2d44c00cea1eefb54bf6b5108b5325c3
suraj <suraj@gmail.com>
2025-04-10 21:01:31 +0530

    imp ques

---
+++ auto-git.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+cd oncdec-b23
+git pull origin main
+git add .
+git commit -m "Automatic Update Repository"
+git push origin main
+
+
+Git Imp Questions:
+Que. diff CVCS and DVCS
+Que. diff between git clone with https and with ssh
+Que. diff git merge and rebase 
+Que. diff git pull and git fetch 
+Que. explain git working (working dir->staging->local repo)
+Que. how will you clone specific branch from github repository?
+     Ans: git clone https://github.com/abhipraydhoble/oncdec-b23.git  -b feature
