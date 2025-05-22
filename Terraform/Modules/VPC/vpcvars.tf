9cd818d006a722b5a137b30f6f27bf3a482340a9
abhipraydhoble <122669982+abhipraydhoble@users.noreply.github.com>
2025-05-22 20:37:03 +0530

    Update vpcvars.tf

--- Terraform/Modules/VPC/vpcvars.tf
+++ Terraform/Modules/VPC/vpcvars.tf
@@ -1,1 +1,17 @@
 
+variable "vpc_cidr_block" {
+    type = string
+  
+}
+
+variable "subnet_cidr_block" {
+  type = string
+}
+
+variable "az" {
+  type = string 
+}
+
+variable "public_ip" {
+  type = bool
+}
