486292fecab6be55550d4f383551a91b52ad8be4
abhipraydhoble <122669982+abhipraydhoble@users.noreply.github.com>
2025-05-22 20:37:53 +0530

    Update ec2.tf

--- Terraform/Modules/EC2/ec2.tf
+++ Terraform/Modules/EC2/ec2.tf
@@ -1,1 +1,12 @@
 
+resource "aws_instance" "vm" {
+    ami = var.ami_id
+    instance_type = var.instance_type
+    key_name = var.key
+    subnet_id = var.subnet_id
+    vpc_security_group_ids = [var.security_group_id]
+    
+    tags = {
+        Name = "Test-Server"
+    }
+}
